91025aeed94dc3168e6a55976553db440432e53d
Adam Jurcisin <jurcisin.adam@gmail.com>
2023-07-05 19:13:24 +0200

    Adding Basic Styles

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/random?nature,forest,campground",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique earum facere quibusdam autem, tenetur accusantium vel odio repellendus. Aliquam alias rerum ad, quam esse qui error est cum unde maiores!",
+            price
         })
         await camp.save();
     }
